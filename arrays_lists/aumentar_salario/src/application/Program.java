76547216b8afd6dde0d3ce1a3009ca913f0391d3
Gustavo Rodrigues Santos Silva <gustavo.rodriguesssbr@gmail.com>
2023-01-11 15:30:12 -0300

    Melhorando o código aumentar_salario

--- arrays_lists/aumentar_salario/src/application/Program.java
+++ arrays_lists/aumentar_salario/src/application/Program.java
@@ -24,55 +24,60 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Integer id;
-		String nome;
-		Double salario;
-		Double percent;
-		Boolean controle = false;
-
-		Funcionario objeto = null;
 		List<Funcionario> lista = new ArrayList<>();
 
 		System.out.print("Quantos Funcionários deseja Digitar? ");
 		int n = sc.nextInt();
+		System.out.println();
 
 		for (int i = 0; i < n; i++) {
 			System.out.println("Funcionário " + (i + 1));
 			System.out.print("ID: ");
-			id = sc.nextInt();
+			Integer id = sc.nextInt();
+			while (verificarId(lista, id)) {
+				System.out.print("ID já existente, digite novamente: ");
+				id = sc.nextInt();
+			}
+
 			System.out.print("NOME: ");
 			sc.nextLine();
-			nome = sc.nextLine();
-			System.out.print("SALÁRIO: ");
-			salario = sc.nextDouble();
+			String nome = sc.nextLine();
 
-			objeto = new Funcionario(id, nome, salario);
+			System.out.print("SALÁRIO: ");
+			Double salario = sc.nextDouble();
+
+			Funcionario objeto = new Funcionario(id, nome, salario);
 			lista.add(objeto);
+			System.out.println();
 		}
 
 		System.out.print("Digite o ID do funcionario que terá o salário aumentado: ");
-		id = sc.nextInt();
-		System.out.print("Digite a porcentagem que vai aumentar: ");
-		percent = sc.nextDouble();
+		Integer id = sc.nextInt();
 
-		for (Funcionario obj : lista) {
-			if (obj.getId() == id) {
-				obj.aumentarSalario(percent);
-				controle = true;
-			}
+		Funcionario obj = lista.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		System.out.println();
+
+		if (obj == null) {
+			System.out.println("Funcionário não encontrado");
+
+		} else {
+			System.out.print("Digite a porcentagem que vai aumentar: ");
+			Double percent = sc.nextDouble();
+			obj.aumentarSalario(percent);
 		}
 
 		System.out.println();
 
-		if (controle == false) {
-			System.out.println("Funcionário não encontrado");
-		}
-
-		for (Funcionario obj : lista) {
-			System.out.println(obj);
+		for (Funcionario func : lista) {
+			System.out.println(func);
 		}
 
 		sc.close();
+	}
+
+	public static boolean verificarId(List<Funcionario> lista, int id) {
+		Funcionario obj = lista.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return obj != null;
 	}
 
 }
