1ee7b105134babfac4d141b4508c809242f53cec
Gustavo Rodrigues Santos Silva <gustavo.rodriguesssbr@gmail.com>
2023-01-13 20:05:21 -0300

    Bubble Sort

---
+++ arrays_lists/bubble_sort/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/*
+		 * Receba um valor N que vai definir o tamanho do Vetor, e preencha esse vetor
+		 * com numeros inteiros, depois imprima o vetor ordenado
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		boolean controle;
+		int aux;
+
+		System.out.print("Digite o tamanho do Vetor: ");
+		int n = sc.nextInt();
+
+		int[] vetor = new int[n];
+
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print("Digite o valor da " + (i + 1) + "° posição do vetor: ");
+			vetor[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < vetor.length; i++) {
+			controle = true;
+			for (int j = 0; j < (vetor.length - 1); j++) {
+				if (vetor[j] > vetor[j + 1]) {
+					aux = vetor[j];
+					vetor[j] = vetor[j + 1];
+					vetor[j + 1] = aux;
+					controle = false;
+				}
+			}
+
+			if (controle) {
+				break;
+			}
+
+		}
+
+		System.out.println();
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+
+		sc.close();
+
+	}
+
+}
