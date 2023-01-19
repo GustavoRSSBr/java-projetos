ede80d70d2173236937f493017eaf93602923c74
Gustavo Rodrigues Santos Silva <gustavo.rodriguesssbr@gmail.com>
2023-01-19 19:59:45 -0300

    atualizando LocalDate

--- estrutura_sequencial/local_date/src/application/Program.java
+++ estrutura_sequencial/local_date/src/application/Program.java
@@ -10,17 +10,21 @@ public class Program {
 
 	public static void main(String[] args) {
 		// Testando LocalDate, LocalDateTime e Instant
-
-		LocalDate d1 = LocalDate.now(); //Pega o horario atual
-		LocalDateTime d2 = LocalDateTime.now();
-		Instant d3 = Instant.now(); //Pega o horario atual de Londres(default de instant)
-		
-		LocalDate d4 = LocalDate.ofInstant(d3, ZoneId.of("Portugal")); // converte instant pra localDate com ZoneId especifico
-		LocalDateTime d5 = LocalDateTime.ofInstant(d3, ZoneId.of("America/Chicago"));
-
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); // formato para Instant
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); // formato
+																													// para
+																													// Instant
+
+		LocalDate d1 = LocalDate.now(); // Pega a data
+		LocalDateTime d2 = LocalDateTime.now();
+		Instant d3 = Instant.now(); // Pega o horario atual de Londres(default de instant)
+
+		LocalDate d4 = LocalDate.ofInstant(d3, ZoneId.of("Portugal")); // converte instant pra localDate com ZoneId
+																		// especifico
+		LocalDateTime d5 = LocalDateTime.ofInstant(d3, ZoneId.of("America/Chicago"));
+		
+		LocalDate d6 = LocalDate.parse("07/10/1999", fmt1); //texto formatado para localdate 
 
 		System.out.println(d1); // imprime data atual
 
@@ -35,6 +39,8 @@ public class Program {
 		System.out.println(fmt1.format(d4)); // Instant -> LocalDate: formatada com o zoneId especifico
 
 		System.out.println(fmt2.format(d5)); // Instant -> LocalDateTime: formatada com o zoneId especifico
+		
+		System.out.println(d6.format(fmt1));
 
 	}
 
