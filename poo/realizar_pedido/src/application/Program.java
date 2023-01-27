c9abbbb4551a323585b98461bdd4de366c5cb612
Gustavo Rodrigues Santos Silva <gustavo.rodriguesssbr@gmail.com>
2023-01-27 19:29:43 -0300

    Realizar pedido finalizado

--- poo/realizar_pedido/src/application/Program.java
+++ poo/realizar_pedido/src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,50 +12,51 @@ import entities.Produto;
 import entities.enums.PedidoStatus;
 
 public class Program {
-	
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Digite os dados do Cliente");
+		System.out.println("Digite os dados do cliente");
 
 		System.out.print("Nome: ");
 		String nome = sc.nextLine();
 		System.out.print("Email: ");
 		String email = sc.nextLine();
-		System.out.print("Data de Nascimento (dd/mm/aaaa) : ");
-		LocalDate data = LocalDate.parse(sc.next(), formato1);
+		System.out.print("Data de nascimento (dd/mm/aaaa) : ");
+		LocalDate data = LocalDate.parse(sc.next(), Pedido.formato1);
 
 		Cliente c1 = new Cliente(nome, email, data);
 
-		System.out.println("Digite os dados do Pedido");
+		System.out.println("Digite os dados do pedido");
 		System.out.print("Status: ");
 		String status = sc.next();
 
 		Pedido pedido = new Pedido(Instant.now(), PedidoStatus.valueOf(status), c1);
 
-		System.out.print("Quantos Itens tem o Pedido? ");
+		System.out.print("Quantos itens tem o pedido? ");
 		int n = sc.nextInt();
 
 		for (int i = 1; i <= n; i++) {
-			System.out.println("Digite os dados do " + i + "° Item");
+			System.out.println("Digite os dados do " + i + "° item");
 			System.out.print("Nome do produto: ");
+			sc.nextLine();
 			String nomeProduto = sc.nextLine();
-			System.out.println("Valor do produto");
+			System.out.print("Valor do produto: ");
 			Double preco = sc.nextDouble();
 
 			Produto p = new Produto(nomeProduto, preco);
 
-			System.out.print("Quantidade ");
+			System.out.print("Quantidade: ");
 			int quantidade = sc.nextInt();
 
 			PedidoItem item = new PedidoItem(quantidade, p);
 
 			pedido.addItem(item);
 		}
-		
-		
+
+		System.out.println();
+		System.out.println(pedido.toString());
 
 		sc.close();
 
--- poo/realizar_pedido/src/entities/Pedido.java
+++ poo/realizar_pedido/src/entities/Pedido.java
@@ -1,6 +1,8 @@
 package entities;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +12,9 @@ public class Pedido {
 
 	private Instant momento;
 	private PedidoStatus status;
+
+	public static DateTimeFormatter formato1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	public static DateTimeFormatter formato2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
 	private Cliente cliente;
 	List<PedidoItem> listaPedido = new ArrayList<>();
@@ -66,6 +71,30 @@ public class Pedido {
 			total += p.subTotal();
 		}
 		return total;
+	}
+
+	public String toString() {
+		StringBuffer texto = new StringBuffer();
+
+		texto.append("INFORMAÇÕES DO PEDIDO");
+		texto.append("\nMomento Do pedido: ");
+		texto.append(momento.atZone(ZoneId.systemDefault()).format(formato2));
+		texto.append("\nStatus do pedido: ");
+		texto.append(getStatus());
+		texto.append("\nCliente: ");
+		texto.append(cliente.getName());
+		texto.append(" (" + cliente.getDataNasc().format(formato1) + ") - ");
+		texto.append(cliente.getEmail());
+		texto.append("\nItens do pedido:\n");
+		for (PedidoItem i : listaPedido) {
+			texto.append(i.getProduto().getNome() + ", ");
+			texto.append("R$" + String.format("%.2f", i.getProduto().getPreco()) + ", ");
+			texto.append("Quantidade: " + i.getQuantidade() + ", ");
+			texto.append("Subtotal: R$" + String.format("%.2f", i.subTotal()) + "\n");
+		}
+		texto.append("\nPreço total: R$" + String.format("%.2f", total()));
+
+		return texto.toString();
 	}
 
 }
