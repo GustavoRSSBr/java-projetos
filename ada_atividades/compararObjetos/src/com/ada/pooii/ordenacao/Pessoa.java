f3aa8adb842d66a2fd42d801dbbb966210eb0fbb
Gustavo Rodrigues Santos Silva <gustavo.rodriguesssbr@gmail.com>
2023-08-25 20:09:53 -0300

    Commit inicial aula

---
+++ ada_atividades/compararObjetos/src/com/ada/pooii/ordenacao/Pessoa.java
@@ -0,0 +1,30 @@
+package com.ada.pooii.ordenacao;
+
+import java.time.LocalDate;
+
+public class Pessoa implements Comparable<Pessoa> {
+
+    private String nome;
+
+    private LocalDate dataDeNascimento;
+
+    public Pessoa(String nome, LocalDate dataDeNascimento) {
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", dataDeNascimento=" + dataDeNascimento +
+                '}';
+    }
+
+    // Fazer a classe Pessoa ser ordenada pelos nomes das pessoas
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return 0;
+    }
+}
