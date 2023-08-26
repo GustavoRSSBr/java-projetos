0110290e0bc80bec0e20a24350e0441f55a76ae9
Gustavo Rodrigues Santos Silva <gustavo.rodriguesssbr@gmail.com>
2023-08-25 21:10:38 -0300

    ordenando por idade

--- ada_atividades/compararObjetos/src/com/ada/pooii/ordenacao/Pessoa.java
+++ ada_atividades/compararObjetos/src/com/ada/pooii/ordenacao/Pessoa.java
@@ -1,16 +1,23 @@
 package com.ada.pooii.ordenacao;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Pessoa implements Comparable<Pessoa> {
 
     private String nome;
 
     private LocalDate dataDeNascimento;
+    
 
     public Pessoa(String nome, LocalDate dataDeNascimento) {
         this.nome = nome;
         this.dataDeNascimento = dataDeNascimento;
+        
+    }
+    
+    public int getIdade() {
+    	return Period.between(dataDeNascimento, LocalDate.now()).getYears();
     }
 
 
@@ -19,13 +26,16 @@ public class Pessoa implements Comparable<Pessoa> {
         return "Pessoa{" +
                 "nome='" + nome + '\'' +
                 ", dataDeNascimento=" + dataDeNascimento +
+                ", idade=" + getIdade() + "Anos" +
                 '}';
     }
 
     // Fazer a classe Pessoa ser ordenada pelos nomes das pessoas
     @Override
     public int compareTo(Pessoa pessoa) {
+    	Integer idade = getIdade();
     	
-        return this.nome.toLowerCase().compareTo(pessoa.nome.toLowerCase());
+        //return this.nome.toLowerCase().compareTo(pessoa.nome.toLowerCase());
+        return idade.compareTo(pessoa.getIdade());
     }
 }
